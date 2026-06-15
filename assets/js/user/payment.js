0dc89ef050753ce068149b9b1885c31fde2e12d9
Frank Wuaten <93098942+SteezyBoy@users.noreply.github.com>
2026-06-15 12:56:44 +0800

    Delete assets directory

--- assets/js/user/payment.js
+++
@@ -1,8 +0,0 @@
-// Tambahkan ini agar error 'is not defined' hilang
-function stopCashierPaymentPoll() {
-    console.log("Polling dihentikan.");
-    // Jika Anda punya variabel interval, clear di sini
-    if (typeof window.paymentInterval !== 'undefined') {
-        clearInterval(window.paymentInterval);
-    }
-}
